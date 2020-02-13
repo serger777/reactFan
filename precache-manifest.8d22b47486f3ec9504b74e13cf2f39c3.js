dcff22636954b923973fc10d970ab1b3ae2b38c3
sergey gerasimov <sergey.gerasimov@realize-it.ru>
2020-02-13 22:52:34 +0300

    Updates

---
+++ precache-manifest.8d22b47486f3ec9504b74e13cf2f39c3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93c077275647288fdba016c9f18c004c",
+    "url": "/reactFan/index.html"
+  },
+  {
+    "revision": "39ee35297b82f9a9334d",
+    "url": "/reactFan/static/css/main.8417ef44.chunk.css"
+  },
+  {
+    "revision": "d5531d83a1515f051998",
+    "url": "/reactFan/static/js/2.a631bfed.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/reactFan/static/js/2.a631bfed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39ee35297b82f9a9334d",
+    "url": "/reactFan/static/js/main.8136ac5d.chunk.js"
+  },
+  {
+    "revision": "16282041f045477ac5ba",
+    "url": "/reactFan/static/js/runtime-main.bddcc678.js"
+  },
+  {
+    "revision": "34723a2fbef5882977e8392d020956af",
+    "url": "/reactFan/static/media/TrebuchetMS.34723a2f.woff"
+  },
+  {
+    "revision": "6f47a89bdb6eb676e6d049309ece6718",
+    "url": "/reactFan/static/media/cat.6f47a89b.png"
+  },
+  {
+    "revision": "63e9d30259c962a3ea5046555ec963b6",
+    "url": "/reactFan/static/media/exo2.0-thin.63e9d302.woff"
+  },
+  {
+    "revision": "fb86e249e966a08245044551420982ab",
+    "url": "/reactFan/static/media/pattern.fb86e249.jpg"
+  }
+]);
